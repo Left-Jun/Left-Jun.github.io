ced7afb75f3fd23d84162f7a03d9f5448c5ffe96
Left-Jun <1508017864@qq.com>
2026-06-03 22:08:00 +0800

    logo

---
+++ apps/site/src/config/brand.ts
@@ -0,0 +1,11 @@
+export const brand = {
+  name: "Left Jun",
+  group: "Limenauts",
+  slogan: "Explore · Create · Beyond",
+  logos: {
+    mark: "/brand/lj-mark.svg",
+    markDark: "/brand/lj-mark-dark.svg",
+    lockup: "/brand/limenauts-lockup.svg",
+    lockupDark: "/brand/limenauts-lockup-dark.svg",
+  },
+} as const;
